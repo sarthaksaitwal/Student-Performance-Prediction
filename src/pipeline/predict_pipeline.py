07d03336d69d06f6bda42d14f049bb53f96ccc91
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2025-12-27 23:46:03 +0530

    Implement Flask web application for student performance prediction with HTML form and CSS styling

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -60,17 +60,17 @@ class CustomData:
         })
 
 
-if __name__ == "__main__":
-    data = CustomData(
-        gender="female",
-        race_ethnicity="group B",
-        parental_level_of_education="bachelor's degree",
-        lunch="standard",
-        test_preparation_course="none",
-        reading_score=72,
-        writing_score=74
-    )
+# if __name__ == "__main__":
+#     data = CustomData(
+#         gender="female",
+#         race_ethnicity="group B",
+#         parental_level_of_education="bachelor's degree",
+#         lunch="standard",
+#         test_preparation_course="none",
+#         reading_score=72,
+#         writing_score=74
+#     )
 
-    df = data.get_data_as_dataframe()
-    predictor = PredictPipeline()
-    print(predictor.predict(df))
+#     df = data.get_data_as_dataframe()
+#     predictor = PredictPipeline()
+#     print(predictor.predict(df))
